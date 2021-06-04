7969137c59d9f33d889bca335a75154d3e6b3e7d
Debora Ermel <debbyermel@gmail.com>
2021-06-04 14:25:08 -0500

    Async concepts

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,6 +1,18 @@
 'use strict';
+///////////////////////////////////////
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+//SYNC CODE
+// Executed line by line, which line waits for the previous line.
+
+//ASYNC CODE
+// Code only is executed after a task that runs in the background finishes
+
+const img = document.querySelector('.dog');
+img.src = 'dog.jpg';
+img.addEventListener('load', function() {
+  img.classList.add('fadeIn');
+});
 
 ///////////////////////////////////////
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
