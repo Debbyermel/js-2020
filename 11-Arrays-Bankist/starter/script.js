7099423efec906a9f5dc51d7190a0a822e64078c
Debora Ermel <debbyermel@gmail.com>
2022-01-13 10:30:59 -0600

    find

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -96,3 +96,23 @@ console.log(arr2.at(2)); // Expected Output: "three"
 console.log(arr2.at(-1)); // Expected Output: false
 console.log(arr2.at(-3)); // Expected Output: 5
 
+
+
+//Find
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+console.log(
+  `Sarah's dog is eating too ${
+    dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+  } `
+);
